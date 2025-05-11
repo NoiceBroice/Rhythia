f1ad278eaff811fccc948d6117652afaab2f619a
Faded <air.jc@proton.me>
2025-05-11 15:04:37 +0900

    WIP: Game loop

---
+++ scripts/game/StandardGame.cs
@@ -0,0 +1,17 @@
+using Godot;
+using Rhythia.Maps;
+using System;
+
+public partial class StandardGame : Node3D
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public void Play(IMap map)
+    {
+        
+    }
+
+}
